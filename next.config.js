d708a21d95c9c8ff8a80534835790b56fbb4bf7c
Papermint Group <sg.mthembu@gmail.com>
2026-01-16 19:56:59 +0200

    Fix: Remove static export to allow getServerSideProps

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // <--- This must be inside the brackets
   images: {
     unoptimized: true,
   },
-  // If your site is at papermint.group/DriverRate, add this:
-  basePath: '/DriverRate', 
 }
 
 module.exports = nextConfig;
